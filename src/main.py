cc2badaf1bc6c4876fe66594d1bdf59594f41955
Asim-Tahir <AsimTahir@protonmail.com>
2021-03-18 21:39:53 +0300

    DotEnv: Added SENTRY_USE env var for Sentry usage

--- src/main.py
+++ src/main.py
@@ -5,6 +5,7 @@ from environ import (
     client_secret,
     bot_username,
     bot_pass,
+    SENTRY_USE,
     SENTRY_DSN,
     SENTRY_TRACES_SAMPLE_RATE,
 )
@@ -13,10 +14,11 @@ from MainWorker import MainWorker
 import sentry_sdk
 
 if __name__ == "__main__":
-    sentry_sdk.init(
-        SENTRY_DSN,
-        traces_sample_rate=SENTRY_TRACES_SAMPLE_RATE,
-    )
+    if SENTRY_USE:
+        sentry_sdk.init(
+            SENTRY_DSN,
+            traces_sample_rate=SENTRY_TRACES_SAMPLE_RATE,
+        )
 
     reverse_img_bot = rBot(useragent, client_id, client_secret, bot_username, bot_pass)
     hash_db = HashDatabase()
